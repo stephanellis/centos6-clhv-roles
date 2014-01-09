5dfa8b330cb35ca34252d6cf5495b4bcee0554b9
Stephan Ellis <stephan@ellisopen.com>
2014-01-09 10:40:28 -0600

    fixed up creation of gluster volume in my lab environment

--- extras/setup-vpool1.sh
+++ extras/setup-vpool1.sh
@@ -12,15 +12,19 @@ ansible abmx-ffio -a "mkdir -p /exports/vpool1b1/b"
 ansible abmx-ffio -a "mkdir -p /exports/vpool1b2/b"
 
 ansible hyper121 -a "gluster peer probe 172.16.17.122"
+sleep 5
 ansible hyper121 -a "gluster peer probe 172.16.17.123"
-
+sleep 5
 # fix the address for hyper121 in the trusted storage pool
 # side effect of the ring topology
 sleep 5
+
 ansible hyper122 -a "gluster peer detach 172.16.16.10"
+sleep 5
+
 ansible hyper122 -a "gluster peer probe 172.16.17.121"
 
-sleep 5
+sleep 10
 
 ansible hyper121 -a "gluster volume create vpool1 replica 2 transport tcp 172.16.17.121:/exports/vpool1b1/b 172.16.17.122:/exports/vpool1b2/b 172.16.17.122:/exports/vpool1b1/b 172.16.17.123:/exports/vpool1b2/b 172.16.17.123:/exports/vpool1b1/b 172.16.17.121:/exports/vpool1b2/b"
 
@@ -35,9 +39,12 @@ ansible hyper121 -a "gluster volume set vpool1 storage.owner-uid 107"
 ansible hyper121 -a "gluster volume set vpool1 storage.owner-gid 107"
 ansible hyper121 -a "gluster volume start vpool1"
 
-ansible abmx-ffio -m copy -a "src=extra/vpool1.xml dest=/tmp/"
+sleep 10
+
+ansible abmx-ffio -m copy -a "src=extras/vpool1.xml dest=/tmp/"
 ansible abmx-ffio -a "virsh pool-define /tmp/vpool1.xml"
 
 ansible abmx-ffio -a "mkdir /var/lib/libvirt/images/vpool1"
 ansible abmx-ffio -a "virsh pool-start vpool1"
 ansible abmx-ffio -a "virsh pool-autostart vpool1"
+
